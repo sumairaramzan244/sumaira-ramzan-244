5a2f3ec48313817657dd88eaa6176aa7a3ee06b1
Sumaira Ramzan <sumaira.ramzan244@gmail.com>
2025-11-01 14:18:32 +0500

    Change base path in Vite config
    Updated the base path for the Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: '/sumaira-ramzan-244/',  // <- change here
   server: {
     host: "::",
     port: 8080,
